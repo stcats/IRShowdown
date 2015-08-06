f7cfdd6c552c3832be87ec45d346f5ae4b1edede
Ivan Maeder <ivan.maeder@gmail.com>
2015-08-06 13:57:31 +0100

    Reliable version transmitting 1 byte

--- IRShowdown.ino
+++ IRShowdown.ino
@@ -1,31 +1,41 @@
-
-// Include Gamer library, as well as the infrared library.
 #include <Gamer.h>
 #include <GamerIR.h>
 #include <SoftwareSerial.h>
 
-// Create a copy of the Gamer library.
 Gamer gamer;
-// Create a copy of the Gamer IR library.
 GamerIR infrared;
 
-// General
-int score;
-boolean isPlaying;
+/* The aim of this game is to shoot before getting shot at, but the trick
+ is that the time to wait before being able to shoot (the time spent
+ during the countdown) is unpredictable.
+
+ The random value used in the countdown is stored in this variable.
+ */
 int delayBeforeShooting;
 
-/*
-    The mode variable keeps track of the mode that the game is in.
+/* The game changes between different states, and the "mode" variable
+ is used to store the current active state.
  
- 0 = waiting for other Gamer.
- 1 = countdown.
- 2 = waiting for shot.
- 3 = shot fired.
+ The possible game states are:
+ 
+     0 = in this state the game displays an image (the "splash screen")
+         and waits for the player to press START
+     1 = the game tries to connect to other players (others that have
+         also pressed START)
+     2 = the countdown begins and, as soon as a shot is fired the result
+         is displayed
+
+ This instruction adds one to the "mode" variable, and therefore advances
+ the game to the next stage:
+
+     mode++;
+
  */
 int mode;
 
-// Splash screen image
-byte splashScreen[8] = {
+/* Images.
+ */
+byte cowboy[8] = {
   B00011000,
   B01111110,
   B00111100,
@@ -36,7 +46,6 @@ byte splashScreen[8] = {
   B11000011
 };
 
-// Go image
 byte go[8] = {
   B11100111,
   B10100101,
@@ -44,73 +53,145 @@ byte go[8] = {
   B10000101,
   B10110101,
   B10100101,
-  B11100111,
-  B00000000
+  B10100101,
+  B11100111
 };
 
 void setup() {
   gamer.begin();
+  randomSeed(gamer.ldrValue());
 }
 
 void loop() {
-  if (isPlaying) {
-    switch (mode) {
+  switch (mode) {
     case 0:
-      // Waiting for other Gamer
-      waitForOtherGamer();
+      introduction();
       break;
 
     case 1:
-      countdown();
+      synchronizeGamers();
       break;
 
     case 2:
-      //detectShot();
+      countdown();
       break;
 
     case 3:
+      detectShot();
       break;
-    }
-  }
-  else {
-    showSplashScreen();
   }
 }
 
-void waitForOtherGamer() {
-  String temp = infrared.receive();
-  if(temp > 0) {
-    // Other Gamer has pressed start before this one. 
-    delayBeforeShooting = temp * 20;
+void introduction() {
+  gamer.printImage(cowboy);
+
+  if (gamer.isPressed(START)) {
+    delayBeforeShooting = 0; //reset
     mode++;
-  } 
-  else {
-    // This is the first Gamer. Send random byte. 
-    infrared.send(byte(random(100, 254)));
   }
+}
+
+/* TODO Because each player runs the same code, 
+ - explain random time to wait for others
+ - explain same code (otherwise one would always transmit to rest)
+ - explain that what's getting broadcast is the random value
+ */
+void synchronizeGamers() {
+  waitForOtherPlayersToJoin();
+
+  if (!broadcastReceived()) {
+    /* We've waited a while and no other Gamer has decided to transmit
+     a random value, so it's up to us.
+     */
+    broadcastMessage();
+  }
+  
+  mode++;
+}
+
+void waitForOtherPlayersToJoin() {
+  delay(100);
+}
+
+boolean broadcastReceived() {
+  /* We would like to wait for a while in case other DIY Gamer Kits
+   decide to broadcast before us.
+
+   But using "delay()" will not do because we need to listen whilst letting
+   the time pass.
+
+   The solution is to loop over and over untile "timeToListen" is greater
+   than the time now ("millis()"), and the time when the loop first started
+   running ("timeWhenListenBegan").
+   */
+  int timeToListen = random(100, 1000);
+  unsigned long timeWhenListenBegan = millis();
+
+  while (millis() < (timeWhenListenBegan + timeToListen)) {
+    String data = infrared.receive();
+
+    if (data.length() > 0) {
+      delayBeforeShooting = data.toInt();
+
+      if (delayBeforeShooting > 0) { //have we received a valid number?
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+void broadcastMessage() {
+  /* A larger range will reduce the reliability because the transmission will
+   require two characters.
+   */
+  delayBeforeShooting = random(1, 9);
+
+  transmit(delayBeforeShooting);
+}
+
+void transmit(int number) {
+  transmit((String) number);
+}
+
+void transmit(String string) {
+  gamer.setLED(1);
+  infrared.send(string);
+  gamer.setLED(0);
 }
 
 void countdown() {
-  gamer.printString("READY");
-  delay(1000);
-  gamer.printString("SET");
-  delay(delayBeforeShooting);
+  gamer.printString("Ready");
+  gamer.printString("Set");
+  delay(delayBeforeShooting * 500);
+  infrared.receive(); //clear any buffered values before detecting shot
   gamer.printImage(go);
 
-  mode = mode + 1;
+  mode++;
 }
 
-/* ---------------------------------------------------------------
- Shows an image when the game isn't being played.
- */
-void showSplashScreen() {
+void detectShot() {
+  String data = infrared.receive();
+  if (data.length() > 0 && data.equals("B")) {
+    gamer.printString("You lose");
+    mode = 0;
+
+    return;
+  }
+
+  if (gamer.isPressed(UP)) {
+    transmit("B"); //B for "Bang!"
+    
+    gamer.printString("You win");  
+    mode = 0;
+  }
+  
+  checkForRestart();
+}
+
+void checkForRestart() {
   if (gamer.isPressed(START)) {
-    isPlaying = true;
-    // Start game!
-  }
-  else {
-    gamer.printImage(splashScreen);
+    mode = 0;
   }
 }
-
-
